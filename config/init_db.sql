8e9c03f2205c301eb54718310998f3512e531273
Artemf29 <57762801+artemf29@users.noreply.github.com>
2021-01-15 02:03:59 +0300

    init_db.sql

--- config/init_db.sql
+++ config/init_db.sql
@@ -1,47 +1,50 @@
-create table person
-(
-    uuid         char(36) not null
-        constraint person_pk
-            primary key,
-    name        text     not null,
-    create_date text     not null,
-    update_date text     not null
-);
-
-create table organization
+create table contacts
 (
     uuid        char(36) not null
-        constraint organization_pk
+        constraint contacts_pk
             primary key,
     name        text     not null,
-    info        text     not null,
     create_date text     not null,
     update_date text     not null
 );
 
 create table phone_number
 (
-    id            serial   not null
+    id           serial   not null
         constraint phone_number_pk
             primary key,
-    contacts_uuid char(36) not null,
-    number        text     not null
+    contact_uuid char(36) not null
+        constraint phone_number_contacts_uuid_fk
+            references contacts
+            on update restrict on delete cascade,
+    number       text     not null
 );
 
-create table gender
+create table type_contact
 (
-    id          serial   not null
-        constraint gender_pk
+    id             serial not null
+        constraint type_contact_pk
             primary key,
-    person_uuid char(36) not null,
-    gender      text     not null
+    name           text   not null,
+    personal_field text   not null
 );
 
-create table contacts
+create table contact_info
 (
-    id       serial   not null
-        constraint contacts_pk
+    id              serial   not null
+        constraint contact_info_pk
             primary key,
-    all_uuid char(36) not null,
-    name     text     not null
+    contact_uuid    char(36) not null
+        constraint contact_info_contacts_uuid_fk
+            references contacts
+            on update restrict on delete cascade,
+    type_contact_id integer  not null
+        constraint contact_info_type_contact_id_fk
+            references type_contact
+            on update restrict on delete set null,
+    do_action       text     not null
 );
+
+insert into type_contact (id, name, personal_field)
+VALUES ('1', 'Person', 'Gender(men/woman)'),
+       ('2', 'Organization', 'Information');
